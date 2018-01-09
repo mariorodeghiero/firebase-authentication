e5877c3708d8f5763cd963aaa2fababe9f8188f9
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-01-09 15:55:29 -0200

    started config files app.js index.html and style.css

---
+++ app.js
@@ -0,0 +1,17 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDEQNHMJ0rhnY-QhSjGxayOXOkIJ0fNB60",
+    authDomain: "my-firebase-authenticati-599c7.firebaseapp.com",
+    databaseURL: "https://my-firebase-authenticati-599c7.firebaseio.com",
+    projectId: "my-firebase-authenticati-599c7",
+    storageBucket: "",
+    messagingSenderId: "1088864274215"
+};
+firebase.initializeApp(config);
+
+const txtEmail = document.getElementById('txtEmail');
+const txtEmail = document.getElementById();
+const txtEmail = document.getElementById();
+const txtEmail = document.getElementById();
+const txtEmail = document.getElementById();
