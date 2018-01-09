784f2043970d2d1528c632e86a1563d480dfe7c4
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-01-09 16:23:44 -0200

    edited app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+( function () {
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyDEQNHMJ0rhnY-QhSjGxayOXOkIJ0fNB60",
@@ -11,7 +11,45 @@ var config = {
 firebase.initializeApp(config);
 
 const txtEmail = document.getElementById('txtEmail');
-const txtEmail = document.getElementById();
-const txtEmail = document.getElementById();
-const txtEmail = document.getElementById();
-const txtEmail = document.getElementById();
+const txtPassword = document.getElementById('txtPassword');
+const btnLogin = document.getElementById('btnLogin');
+const btnSignUp = document.getElementById('btnSignUp');
+const btnLogout = document.getElementById('btnLogout');
+
+btnLogin.addEventListener('click', e => {
+    //Get email and password
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+
+    //Sign in
+
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.cath(e => console.log(e.message));
+})
+
+btnSignUp.addEventListener('click', e => {
+    //Check for real email
+
+    //Get email and password
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+
+    //Sign in
+
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.cath(e => console.log(e.message));
+});
+
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    if (firebaseUser) {
+        console.log(firebaseUser);
+    } else {
+        console.log('------------------------------------');
+        console.log('not logged in');
+        console.log('------------------------------------');
+    }
+})
+
+}());
